334e7971e19cbc0845767541322fdf11cb83f0df
Sadegh Ranjbar <s.ranjbar@dataak.com>
2025-01-16 20:22:53 +0330

    feat: add argparse arguments

---
+++ fastapi-cli.py
@@ -0,0 +1,31 @@
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description="FastAPI CLI, Helper tool for FastAPI projects")
+    subparsers = parser.add_subparsers(dest="command")
+
+    # Command: init
+    init_parser = subparsers.add_parser("init", help="Initialize a new FastAPI project")
+    init_parser.add_argument("project_name", help="Name of the project")
+
+    # Command: add
+    add_parser = subparsers.add_parser("add", help="Add a new component to the project")
+    add_parser.add_argument("component_type", choices=["routers", "models", "schemas", "services", "utils"],
+                            help="Type of component to add")
+    add_parser.add_argument("component_name", help="Name of the component")
+    add_parser.add_argument("project_name", help="Name of the existing project")
+
+    args = parser.parse_args()
+
+    if args.command == "init":
+        print(f"Init command executed with args: {args.project_name}")
+    elif args.command == "add":
+        print("Add command executed with args: ", args.component_type, args.component_name, args.project_name)
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
